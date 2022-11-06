294c267e856869f9f6d491489a59e985a085756c
Shuyuan Fan <shuyuan.fan@tuhh.de>
2022-11-06 12:07:53 +0000

    Update Linear_optimal.m

--- simulation_file/linear_case/Linear_optimal.m
+++ simulation_file/linear_case/Linear_optimal.m
@@ -1,3 +1,12 @@
+%---------------------------------------------------------------------------------------------
+% For Paper, 
+% "Improving Closed-Loop Dynamic Performance By Assigning Nonlinear Behaviors: Taking Advantages of Concavity Based on Lyapunov Function Methods"
+% by Shuyuan Fan and Herbert Werner
+% Copyright (c) Institute of Control Systems, Hamburg University of Technology. All rights reserved.
+% Licensed under the GPLv3. See LICENSE in the project root for license information.
+% Author(s): Shuyuan Fan (shuyuan.fan@tuhh.de, shuyuanfan1993@gmail.com)
+%--------------------------------------------------------------------------------------------
+
 %% parameters
 beta= [0.5;2;2;0.5;1];
 % beta1 = max u; beta2 = time; beta3 = steady state error
