2950f583b0b91e9c5d7f2029689f23a314ea31b8
Shuyuan Fan <shuyuan.fan@tuhh.de>
2022-11-06 12:06:49 +0000

    Update Fcrddot1.m

--- simulation_file/linear_case/Fcrddot1.m
+++ simulation_file/linear_case/Fcrddot1.m
@@ -1,3 +1,12 @@
+%---------------------------------------------------------------------------------------------
+% For Paper, 
+% "Improving Closed-Loop Dynamic Performance By Assigning Nonlinear Behaviors: Taking Advantages of Concavity Based on Lyapunov Function Methods"
+% by Shuyuan Fan and Herbert Werner
+% Copyright (c) Institute of Control Systems, Hamburg University of Technology. All rights reserved.
+% Licensed under the GPLv3. See LICENSE in the project root for license information.
+% Author(s): Shuyuan Fan (shuyuan.fan@tuhh.de, shuyuanfan1993@gmail.com)
+%--------------------------------------------------------------------------------------------
+
 function y = Fcrddot1(xi,ai,bi,ri,ci,di)
 % a,b,c,d>0, x^r>0
 % a>b concave function
