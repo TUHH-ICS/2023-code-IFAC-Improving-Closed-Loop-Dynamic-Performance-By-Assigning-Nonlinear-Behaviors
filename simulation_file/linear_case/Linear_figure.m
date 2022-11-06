9176040ca9933471be8d572449ebeeb0a72b9a3a
Shuyuan Fan <shuyuan.fan@tuhh.de>
2022-11-06 12:07:33 +0000

    Update Linear_figure.m

--- simulation_file/linear_case/Linear_figure.m
+++ simulation_file/linear_case/Linear_figure.m
@@ -1,3 +1,12 @@
+%---------------------------------------------------------------------------------------------
+% For Paper, 
+% "Improving Closed-Loop Dynamic Performance By Assigning Nonlinear Behaviors: Taking Advantages of Concavity Based on Lyapunov Function Methods"
+% by Shuyuan Fan and Herbert Werner
+% Copyright (c) Institute of Control Systems, Hamburg University of Technology. All rights reserved.
+% Licensed under the GPLv3. See LICENSE in the project root for license information.
+% Author(s): Shuyuan Fan (shuyuan.fan@tuhh.de, shuyuanfan1993@gmail.com)
+%--------------------------------------------------------------------------------------------
+
 %%
 load('C:\Users\fan\OneDrive - my.swjtu.edu.cn\CLDBNA\linea_case_withd.mat')
 
